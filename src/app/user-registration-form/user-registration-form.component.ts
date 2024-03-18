b3e33dcc3e432b71a73095346a31ffa9af49da1a
Alex Sweeney <ajsweeney2324@gmail.com>
2024-03-17 19:42:30 -0500

    reinstalling angular material ui

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import { UserRegistrationService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+import { UserRegistrationService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-user-registration-form',
